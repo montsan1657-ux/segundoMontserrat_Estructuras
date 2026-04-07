4b9ffd1780cddc095dc1b8b82ad1f79270c0a161
Montserrat Padilla <montsan1657@gmail.com>
2026-04-07 13:55:22 -0600

    version 1

--- main.py
+++ main.py
@@ -9,3 +9,7 @@ def main():
     pt.alojarPersonas()
     
 
+# 1. yo digo que el atributo de nombre, material, altura y estado, en si todas, ya que para todo se ocupa o tiene lo las cosas para responder esas preguntas 
+# 2. la ventaja es que no es necesario repetir mucho codigo ya que lo hereda y nos ahorramos tiempo
+# 3. tendria los mismo que estructura, solo le añadiria produnfidad tal vez y resistencia en metodos 
+# 4. creo que en el uso de un objeto para orientar a los otros, de esta manera estamos generalizando 
